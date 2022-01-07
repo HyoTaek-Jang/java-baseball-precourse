1349bd4bc088638ef2f7c4ecc8f122771fdc672a
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-01-07 21:00:08 +0900

    setting to pair programming

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,9 @@
 package baseball;
 
-import baseball.computer.Computer;
-import baseball.game.Game;
-import baseball.user.User;
 
 public class Application {
 	public static void main(String[] args) {
 		//TODO: 숫자 야구 게임 구현
-		Game game = new Game(new Computer(), new User());
-		game.start();
+
 	}
 }
